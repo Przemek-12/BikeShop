573c0fe546a16a84a574f815e4bea7f7016c9653
Przemek-12 <pecework@gmail.com>
2020-03-23 19:13:47 +0100

    comments

--- src/test/java/com/example/demo/BootDemoApplicationTests.java
+++ src/test/java/com/example/demo/BootDemoApplicationTests.java
@@ -5,10 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class) 
-@SpringBootTest
+@RunWith(SpringRunner.class) //points on mechanism used to run tests
+@SpringBootTest //runs SpringApplication.run() in main method
 class BootDemoApplicationTests {
 
+	//checks if spring application context was loaded
+	//Spring contexts are also called Spring IoC containers, 
+	//which are responsible for instantiating, configuring, 
+	//and assembling beans by reading configuration metadata from XML, 
+	//Java annotations, and/or Java code in the configuration files.
 	@Test
 	void contextLoads() {
 	}
